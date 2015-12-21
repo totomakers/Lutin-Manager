5b77e17654ab11844c12ffc2d5c78a4662712451
Erwan <d146lemorouxe@128-25.ad.campus-eni.fr>
2015-12-21 14:24:58 +0100

    creation table order

---
+++ database/migrations/2015_12_21_131402_create_order_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->integer('id');
+            $table->datetime('date');
+            $table->string('name');
+            $table->string('address');
+            $table->integer('userId');
+            $table->integer('status');
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('order');
+    }
+}
