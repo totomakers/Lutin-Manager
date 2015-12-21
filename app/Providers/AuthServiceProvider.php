52be93ab7618979192b85f2d3cc1a61781b80aa1
James <jamesprevot@gmail.com>
2015-12-21 14:28:40 +0100

    Suppression d'un namespace inexistant dans AuthServer
    Signed-off-by: James <jamesprevot@gmail.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Providers\AccountServiceProvider;
-use App\User;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
