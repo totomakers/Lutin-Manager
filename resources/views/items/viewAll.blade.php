172b127bb77215bc547d2da65fe7b4dcc366efc8
wbezou <william.bezou@gmail.com>
2015-12-22 16:43:07 +0100

    * Liste des articles
    Signed-off-by: wbezou <william.bezou@gmail.com>

--- resources/views/items/viewAll.blade.php
+++ resources/views/items/viewAll.blade.php
@@ -2,7 +2,31 @@
 @section('title', 'Connexion')
 
 @section('content')
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="row-fluid">
+                <div class="col-xs-offset-1 col-xs-10">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                        <tr class="text-center">
+                            <th>Libell√©</th>
+                            <th>Poids</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
 
-    <!-- liste donnÈe sous la forme $items, avec variable bool $error et $messages -->
-
+                        @foreach($items as $item)
+                            <tr>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->weight }}</td>
+                                <td> Edit | Delete</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
