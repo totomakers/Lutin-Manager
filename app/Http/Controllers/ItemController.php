783b9dfd52258a3b9374fba8adb64900f84f869a
wbezou <william.bezou@gmail.com>
2015-12-22 09:58:09 +0100

    * item controller * ajout statut active 0/1 pour item & user
    Signed-off-by: wbezou <william.bezou@gmail.com>

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -16,11 +16,9 @@ class ItemController extends Controller
 
     public function listAll()
     {
-        $items=Item::all();
+        $items=Item::where('active', 1);
 
-        return view();
-
-
+        return view('items.viewAll',['items' => $items]);
     }
 
     public function add(Request $request)
@@ -48,17 +46,53 @@ class ItemController extends Controller
             //erreur
             return;
         }
-
         return redirect()->route('items::viewAll');
     }
 
-    public function edit($item)
+    public function edit(Request $request)
     {
+        //rules to apply of each field
+        $rulesItem = array(
+            'id'                => 'integer|required',
+            'name'             => 'string|required',
+            'weight'            => 'integer|required|min:0',
+        );
 
+        $validatorItem = Validator::make($request->all(), $rulesItem);
+        if ($validatorItem->fails()) {
+            $errors = $validatorItem->messages()->getMessages();
+            return;
+        }
+
+        $id=Request::input('id');
+        $name=Request::input('name');
+        $weight=Request::input('weight');
+
+        $item=Item::find($id);
+        if($item==null)
+        {
+            //erreur
+        }
+        else
+        {
+            $item->name=$name;
+            $item->weight=$weight;
+            $item->save();
+        }
+        return redirect()->route('items::viewAll');
     }
 
-    public function delete($item)
+    public function delete($id)
     {
-
+        $item=Item::find($id);
+        if($item==null)
+        {
+            //erreur
+        }
+        else
+        {
+            $item->active=0;
+        }
+        return redirect()->route('items::viewAll');
     }
 }
---
+++ resources/views/items/viewAll.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('title', 'Connexion')
+
+@section('content')
+
+    <!-- liste donnée sous la forme $items -->
+
+@endsection
