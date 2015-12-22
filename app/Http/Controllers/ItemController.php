32685b017a96d699cd95ba77c508668c991c4e8e
wbezou <william.bezou@gmail.com>
2015-12-22 08:50:11 +0100

    * item controller - add function
    Signed-off-by: wbezou <william.bezou@gmail.com>

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,16 +5,60 @@ namespace App\Http\Controllers;
 use Validator;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Lang;
-use Carbon\Carbon;
+
 
 use App\Models\Item;
-use App\Models\Order;
-use App\Models\OrderRow;
-use App\Models\User;
 
 
 class ItemController extends Controller
 {
     public function __construct() { }
+
+    public function listAll()
+    {
+        $items=Item::all();
+
+        return view();
+
+
+    }
+
+    public function add(Request $request)
+    {
+        //rules to apply of each field
+        $rulesItem = array(
+            'name'             => 'string|required',
+            'weight'            => 'integer|required|min:0',
+        );
+
+        $validatorItem = Validator::make($request->all(), $rulesItem);
+        if ($validatorItem->fails()) {
+            $errors = $validatorItem->messages()->getMessages();
+            return;
+        }
+
+        $name=Request::input('name');
+        $weight=Request::input('weight');
+
+        $item=new Item();
+        $item->name=$name;
+        $item->weight=$weight;
+        if (!($item->save()))
+        {
+            //erreur
+            return;
+        }
+
+        return redirect()->route('items::viewAll');
+    }
+
+    public function edit($item)
+    {
+
+    }
+
+    public function delete($item)
+    {
+
+    }
 }
