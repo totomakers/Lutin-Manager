854e4d16be2440bdde7f74cf21279bbddeed058b
wbezou <william.bezou@gmail.com>
2015-12-22 12:03:12 +0100

    * Ajout fichier constants pour codes d'erreurs et autre * Ajout fichiers langues pour items et orders (incomplets)
    Signed-off-by: wbezou <william.bezou@gmail.com>

---
+++ app/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Constants
+{
+    const MSG_ERROR_CODE=0;
+    const MSG_WARNING_CODE=1;
+    const MSG_OK_CODE=2;
+    const DEFAULT_USER_ID = 0;
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,7 +29,7 @@ Route::group(['as' => 'orders', 'prefix' => 'orders'], function () //plurial is 
 Route::group(['as' => 'items', 'prefix' => 'items'], function()
 {
     Route::get('/',                     ['as' => '::viewAll', 'middleware' => 'auth.rank:1', 'uses' => 'ItemController@viewAll']);
-    Route::put('/{id}',                 ['as' => '::update',  'middleware' => 'auth.rank:1', 'uses' => 'ItemController@update'])->where('id', '[0-9]+');
+    Route::put('/',                     ['as' => '::update',  'middleware' => 'auth.rank:1', 'uses' => 'ItemController@update']);
     Route::delete('/{id}',              ['as' => '::delete',  'middleware' => 'auth.rank:1', 'uses' => 'ItemController@delete'])->where('id', '[0-9]+');
     Route::post('/',                    ['as' => '::create',  'middleware' => 'auth.rank:1', 'uses' => 'ItemController@create']);
 });
