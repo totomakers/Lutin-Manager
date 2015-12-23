6f1f746e3e2fe1430ac9d0bcb5e35065e6c4e3ad
jchrabie <joel.chrabie2015@campus-eni.fr>
2015-12-23 09:01:01 +0100

    Création de la vue Employé
    Signed-off-by: jchrabie <joel.chrabie2015@campus-eni.fr>

--- resources/views/users/order.blade.php
+++ resources/views/users/order.blade.php
@@ -4,8 +4,8 @@
 @section('content')
 
     @extends('user.header')
-    <div class="container-fluid">
-        <div class="row-fluid">
+    <div class="container-fluid ">
+        <div class="row-fluid ">
             <div class=" col-xs-2">
                 <div class="well well-lg text-center">
                     <p><b>Numéro de commande</b></p>
@@ -53,21 +53,9 @@
                         <div class="tab-tr col-xs-3">
                             <input type="number" class="input-group">
                         </div>
-
                     @endforeach
                 </div>
             </div>
-
         </div>
     </div>
-    Details de la commande num {{ $order->id }}:<br/>
-    Date: {{ (new Carbon($order->date))->formatLocalized('%d %B %Y') }} <br/>
-    nom du client: {{ $order->name }} <br/>
-    Adresse: {{ $order->address }} <br/>
-    Assigné à : {{ $order->user->name }} <br/>
-    <br/>
-    Details:<br/>
-    @foreach($order->rows as $row)
-        {{ $row->item->name }} x {{ $row->quantity }}<br/>
-    @endforeach
 @endsection
