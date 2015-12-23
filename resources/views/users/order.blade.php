98bfa3b4b44d0397b3033d01aa0b342db1cee068
jchrabie <joel.chrabie2015@campus-eni.fr>
2015-12-23 09:36:08 +0100

    Signed-off-by: jchrabie <joel.chrabie2015@campus-eni.fr>

--- resources/views/users/order.blade.php
+++ resources/views/users/order.blade.php
@@ -3,57 +3,59 @@
 
 @section('content')
 
-    @extends('user.header')
+    @extends('users.header')
     <div class="container-fluid ">
-        <div class="row-fluid ">
-            <div class=" col-xs-2">
-                <div class="well well-lg text-center">
-                    <p><b>Numéro de commande</b></p>
+        <div class="user-view">
+            <div class="row-fluid ">
+                <div class=" col-xs-2">
+                    <div class="well well-lg text-center">
+                        <p><b>Numéro de commande</b></p>
 
-                    <p>{{ $order->id }}</p>
+                        <p>{{ $order->id }}</p>
 
-                    <p><b>Nom</b></p>
+                        <p><b>Nom</b></p>
 
-                    <p>{{ $order->name }}</p>
+                        <p>{{ $order->name }}</p>
 
-                    <p><b>Adresse</b></p>
+                        <p><b>Adresse</b></p>
 
-                    <p>{{ $order->address }}</p>
+                        <p>{{ $order->address }}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-10">
+            <div class="col-xs-10">
 
-            <div class="table">
-                <div class="tab-head">
-                    <div class="tab-tr col-xs-3">
-                        Article
-                    </div>
-                    <div class="tab-tr col-xs-3">
-                        Poid
-                    </div>
-                    <div class="tab-tr col-xs-3">
-                        Quantité
-                    </div>
-                    <div class="tab-tr col-xs-3">
-                        Saisie
-                    </div>
-                </div>
-                <div class="tab-body">
-                    @foreach($order->rows as $row)
+                <div class="table">
+                    <div class="tab-head">
                         <div class="tab-tr col-xs-3">
-                            {{ $row->item->name }}
+                            Article
                         </div>
                         <div class="tab-tr col-xs-3">
-                            {{ $row->item->weight }}
+                            Poid
                         </div>
                         <div class="tab-tr col-xs-3">
-                            x{{ $row->quantity }}
+                            Quantité
                         </div>
                         <div class="tab-tr col-xs-3">
-                            <input type="number" class="input-group">
+                            Saisie
                         </div>
-                    @endforeach
+                    </div>
+                    <div class="tab-body">
+                        @foreach($order->rows as $row)
+                            <div class="tab-tr col-xs-3">
+                                {{ $row->item->name }}
+                            </div>
+                            <div class="tab-tr col-xs-3">
+                                {{ $row->item->weight }}
+                            </div>
+                            <div class="tab-tr col-xs-3">
+                                x{{ $row->quantity }}
+                            </div>
+                            <div class="tab-tr col-xs-3">
+                                <input type="number" class="input-group">
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
